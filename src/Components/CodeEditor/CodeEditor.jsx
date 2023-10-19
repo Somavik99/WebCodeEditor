55c2efb5b784935b2a35cb2b532eb3671f1dec29
Avik Roy <somavik99@gmail.com>
2023-10-19 11:45:01 +0530

    Code Editor Start

---
+++ src/Components/CodeEditor/CodeEditor.jsx
@@ -0,0 +1,9 @@
+const HtmlEditor = () => {
+  return (
+    <div>
+      <div className="text-[50px]">Hello World</div>
+    </div>
+  );
+};
+
+export default HtmlEditor;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import HtmlEditor from "../CodeEditor/CodeEditor";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <div>
+            <HtmlEditor/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
